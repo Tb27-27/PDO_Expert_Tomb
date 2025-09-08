4e24f523efcea828f565c84046ba122004fde66a
Tb27-27 <2220046@talnet.nl>
2025-09-08 11:28:07 +0200

    register user errors fix

--- Winkel/user/register-user.php
+++ Winkel/user/register-user.php
@@ -6,20 +6,20 @@
             $user = new User();
 
             // XSS Forcefield
-            $username = htmlspecialchars($_POST['']);
-            $email = htmlspecialchars($_POST['']);
-            $password = htmlspecialchars($_POST['']);
+            $email = htmlspecialchars($_POST['email']);
+            $username = htmlspecialchars($_POST['username']);
+            $password = htmlspecialchars($_POST['password']);
         
-            $user->registerUser($name, $email, $password);
+            $user->registerUser($username, $email, $password);
 
-            // voeg hier nog wat mooie classes toe
+            // FIXME: voeg hier nog wat mooie classes toe
             echo "Registratie gelukt";
-            header("refresh:4, url = ../index.php");
+            header("refresh:10, url = ../index.php");
         }
     }
     catch (Exception $e) {
 
-        // niet heel mooi voor de gebruiker
+        // FIXME: niet heel mooi voor de gebruiker
         echo $e->getMessage();
     }
 
@@ -33,12 +33,13 @@
     <title>Register User</title>
 </head>
 <body>
+    <!-- FIXME: nog geen styling -->
     <h3>Account aanmaken</h3>
     <form method="POST">
         <input type="email" name="email" placeholder="Email" required>
         <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
-        <input type="text">
+        <input type="submit">
     </form>
 </body>
 </html>
