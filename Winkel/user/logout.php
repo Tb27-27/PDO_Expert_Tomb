97ef9cf60d6eb3b3953764cf05f9b6ba1f3af67c
Tb27-27 <2220046@talnet.nl>
2025-09-09 13:16:24 +0200

    fixes voor logout, plus begin van logout css

--- Winkel/user/logout.php
+++ Winkel/user/logout.php
@@ -1,17 +1,31 @@
-<?php
-    require "../includes/user-class.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/stylesheet.css">
+</head>
+<body>
+    <?php
+        require "../includes/user-class.php";
 
-    try {   
-        $user = new User();
+        try {   
+            $user = new User();
 
-        $user->logOutUser();
-        
-        echo "<h3 class='user_h3'>log out gelukt</h3>";
-        header("refresh:5, url = ./login-user.php");
-    } catch (Exception $e) {
+            $user->logOutUser();
+            
+            echo "
+            <div class='user_container'>
+                <h3 class='user_h3'>log out gelukt</h3>
+                </div>
+                ";
+            header("refresh:5, url = ./login-user.php");
+        } catch (Exception $e) {
 
-        // FIXME: niet heel mooi voor de gebruiker
-        echo $e->getMessage();
-    }
-
-?>
+            // FIXME: niet heel mooi voor de gebruiker
+            echo $e->getMessage();
+        }
+    ?>
+</body>
+</html>
