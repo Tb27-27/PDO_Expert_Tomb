cde352175d897367c1f5337213151b9f204aada5
Tb27-27 <2220046@talnet.nl>
2025-09-22 12:43:23 +0200

    pdo product detail pagina

---
+++ Winkel/frontpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frontpage Winkel</title>
+    <link rel="stylesheet" href="./css/">
+</head>
+<body>
+    <h1>Welcome to the shop made from the assignments from PDO: Expert</h1>
+    <h2>Made by:    Tom Bijsterbosch</h2>
+    <h2>Class:      OITSDO24A</h2>
+    <button class="frontpage_button">
+        <a href="./user/login-user.php">Log in</a>
+    </button>
+</body>
+</html>
---
+++ Winkel/product/product.php
@@ -0,0 +1,19 @@
+<?php
+
+require "../includes/product-class.php"
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= $productName ?>
+    </title>
+</head>
+<body>
+    
+</body>
+</html>
